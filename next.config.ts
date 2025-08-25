0a2892854e1804cd676478ecce5fec4a380f0869
KwanleNdaba <kwanele.ndaba@hisgroup-it.co.za>
2025-08-25 11:41:56 +0200

    feat(up)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['randomuser.me'],
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
+
